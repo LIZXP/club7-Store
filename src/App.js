599ea47244f9b689b16d4ece4accdb0318bdc6ac
LIZXP <Peter.xli@outlook.com>
2022-11-06 15:33:59 -0700

    add background image for all the categories

--- src/App.js
+++ src/App.js
@@ -5,30 +5,43 @@ const App = () => {
     {
       id: 1,
       title: "Hats",
+      imageUrl:
+        "https://images.pexels.com/photos/1878821/pexels-photo-1878821.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       id: 2,
       title: "Jackets",
+      imageUrl:
+        "https://images.pexels.com/photos/7270921/pexels-photo-7270921.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       id: 3,
       title: "Sneakers",
+      imageUrl:
+        "https://fashionmagazine.com/wp-content/uploads/2018/02/Dad-Sneakers-001-480x320-c-top.jpg",
     },
     {
       id: 4,
       title: "Womens",
+      imageUrl:
+        "https://images.pexels.com/photos/179909/pexels-photo-179909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       id: 5,
       title: "Mens",
+      imageUrl:
+        "https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
   ];
 
   return (
     <div className="categories-container">
-      {categories.map(({ id, title }) => (
+      {categories.map(({ id, title, imageUrl }) => (
         <div key="id" className="category-container">
-          <div className="background-image" />
+          <div
+            className="background-image"
+            style={{ backgroundImage: `url(${imageUrl})` }}
+          />
           <div className="category-body-container">
             <h2>{title}</h2>
             <p>Shop Now</p>
