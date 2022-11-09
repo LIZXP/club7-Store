f0d88363eb814c8c9966f07e50c4f13fb8e15990
LIZXP <Peter.xli@outlook.com>
2022-11-09 02:43:21 -0700

    create nav bar and apply the style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-
 import Home from "./routes/home/home.component";
+import Nav from "./routes/nav/nav.component";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Nav />}>
+        {/*using index=true is when show the local:3000 the nest route will show the same time */}
+        <Route index={true} element={<Home />} />
+      </Route>
     </Routes>
   );
 };
