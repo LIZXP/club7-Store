a40b399d167cb5215d4afe808ad905501fffb249
LIZXP <Peter.xli@outlook.com>
2023-01-23 23:30:37 -0700

    add in toggle open/hide shopping cart

--- src/componets/cart-icon/cart-icon.jsx
+++ src/componets/cart-icon/cart-icon.jsx
@@ -1,12 +1,19 @@
 import "./cart-icon.styles.scss";
+import { useContext } from "react";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
-
+import { CartContext } from "../../contexts/cart.context";
 import React from "react";
 
 function CartIcon() {
+  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
   return (
     <div className="cart-icon-container">
-      <ShoppingIcon className="shopping-icon" />
+      <ShoppingIcon
+        className="shopping-icon"
+        onClick={() => {
+          setIsCartOpen(!isCartOpen);
+        }}
+      />
       <span className="item-count">0</span>
     </div>
   );
