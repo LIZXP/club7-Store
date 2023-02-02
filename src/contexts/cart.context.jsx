9e812f3e527e5325141543f78396dadc3c573f6e
LIZXP <Peter.xli@outlook.com>
2023-02-02 11:56:11 -0700

    add total reducer function

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -44,6 +44,7 @@ export const CartContext = createContext({
   cartItems: [],
   addItemToCart: () => {},
   cartCount: 0,
+  cartTotal: 0,
   removeItemToCart: () => {},
   clearItemFromCart: () => {},
 });
@@ -52,12 +53,20 @@ export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
   const [cartCount, setCartCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
 
   useEffect(() => {
     const newCartCount = cartItems.reduce((total, cartItem) => {
       return total + cartItem.quantity;
     }, 0);
     setCartCount(newCartCount);
+  }, [cartItems]);
+
+  useEffect(() => {
+    const newCartTotal = cartItems.reduce((total, cartItem) => {
+      return total + cartItem.quantity * cartItem.price;
+    }, 0);
+    setCartTotal(newCartTotal);
   }, [cartItems]);
 
   const addItemToCart = (productToAdd) => {
@@ -76,6 +85,7 @@ export const CartProvider = ({ children }) => {
     setIsCartOpen,
     cartItems,
     cartCount,
+    cartTotal,
     addItemToCart,
     removeItemToCart,
     clearItemFromCart,
