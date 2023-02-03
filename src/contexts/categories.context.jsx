9cb516b4f2541c866ad5fb541de05b5b7c656556
LIZXP <Peter.xli@outlook.com>
2023-02-03 10:34:41 -0700

    create firestore data and change create the product preview page of 4 of each item

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect, createContext } from "react";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+
+export const CategoriesContext = createContext({
+  CategoriesMap: [],
+  setCategoriesMap: () => {},
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments("categories");
+      console.log(categoryMap);
+      setCategoriesMap(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
+
+  const value = { categoriesMap };
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { useContext } from "react";
-import { ProductsContext } from "../../contexts/products.context";
-import ProductCard from "../../componets/product-card/product-card.component";
+import { CategoriesContext } from "../../contexts/categories.context";
+import CategoryPreview from "../../componets/category-preview/category-preview";
 import "./shop.styles.scss";
 
 function Shop() {
-  const { products } = useContext(ProductsContext);
+  const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <div className="products-container">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+    <div className="shop-container">
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
     </div>
   );
 }
