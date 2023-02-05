0931c96f5b14e778a2b7e8f1048e924415047078
LIZXP <Peter.xli@outlook.com>
2023-02-04 23:03:37 -0700

    use useReducer into cart and user context file

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,13 +1,13 @@
-import { useState, useEffect, useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { createContext } from "react";
 import {
   createUserDocFromAuth,
   onAuthStateChangedListener,
 } from "../utils/firebase/firebase.utils";
+import { createAction } from "../utils/reducer/reducer.util";
 
 export const UserContext = createContext({
   currentUser: null,
-  setCurrentUser: () => null,
 });
 
 export const USER_ACTION_TYPES = {
@@ -15,8 +15,6 @@ export const USER_ACTION_TYPES = {
 };
 
 const userReducer = (state, action) => {
-  console.log("dispatched");
-  console.log(action);
   const { type, payload } = action;
   switch (type) {
     case USER_ACTION_TYPES.SET_CURRENT_USER:
@@ -36,9 +34,8 @@ const INITIAL_STATE = {
 export const UserProvider = ({ children }) => {
   const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
   const { currentUser } = state;
-  console.log("current user!!!", currentUser);
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
   };
   const value = { currentUser, setCurrentUser };
 
